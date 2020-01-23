dc4ab5f984fcd49611df3228e5e479a8afb95a78
Anis Redjdal <nissow301@gmail.com>
2020-01-23 14:42:00 -0500

    first commit debut partie 1

---
+++ assets/scripts/1-preproc.js
@@ -0,0 +1,89 @@
+"use strict";
+
+/**
+ * Fichier permettant de traiter les données provenant du fichier CSV.
+ */
+
+
+/**
+ * Précise le domaine en associant un nom de rue à une couleur précise.
+ *
+ * @param color   Échelle de 10 couleurs.
+ * @param data    Données provenant du fichier CSV.
+ */
+function domainColor(color, data) {
+  // TODO: Définir le domaine de la variable "color" en associant un nom de rue à une couleur.
+  var rues = Object.keys(data[0]);
+  color.domain(rues);
+}
+
+/**
+ * Convertit les dates se trouvant dans le fichier CSV en objet de type Date.
+ *
+ * @param data    Données provenant du fichier CSV.
+ * @see https://www.w3schools.com/jsref/jsref_obj_date.asp
+ */
+function parseDate(data) {
+  // TODO: Convertir les dates du fichier CSV en objet de type Date.
+  var parser = d3.timeParse("%d/%m/%Y"); //J ai trouvé ça la dessus http://learnjsdata.com/time.html
+  for (var i = 0; i<data.length; i++) {
+      data[i].Date = parser(data[i].Date);
+  }
+}
+
+/**
+ * Trie les données par nom de rue puis par date.
+ *
+ * @param color     Échelle de 10 couleurs (son domaine contient les noms de rues).
+ * @param data      Données provenant du fichier CSV.
+ *
+ * @return Array    Les données triées qui seront utilisées pour générer les graphiques.
+ *                  L'élément retourné doit être un tableau d'objets comptant 10 entrées, une pour chaque rue
+ *                  et une pour la moyenne. L'objet retourné doit être de la forme suivante:
+ *
+ *                  [
+ *                    {
+ *                      name: string      // Le nom de la rue,
+ *                      values: [         // Le tableau compte 365 entrées, pour les 365 jours de l'année.
+ *                        date: Date,     // La date du jour.
+ *                        count: number   // Le nombre de vélos compté ce jour là (effectuer une conversion avec parseInt)
+ *                      ]
+ *                    },
+ *                     ...
+ *                  ]
+ */
+function createSources(color, data) {
+  // TODO: Retourner l'objet ayant le format demandé.
+  for (var i = 0; i < color.domain().length(); i++) {
+    var rue = color.domain()[i];
+    color.domain()[i] = { "name": color.domain()[i], "values" : []};
+  } 
+  //for (var i = 0; i < ) {
+//
+  //}
+  return color.domain();
+}
+
+/**
+ * Précise le domaine des échelles utilisées par les graphiques "focus" et "contexte" pour l'axe X.
+ *
+ * @param xFocus      Échelle en X utilisée avec le graphique "focus".
+ * @param xContext    Échelle en X utilisée avec le graphique "contexte".
+ * @param data        Données provenant du fichier CSV.
+ */
+function domainX(xFocus, xContext, data) {
+  // TODO: Préciser les domaines pour les variables "xFocus" et "xContext" pour l'axe X.
+
+}
+
+/**
+ * Précise le domaine des échelles utilisées par les graphiques "focus" et "contexte" pour l'axe Y.
+ *
+ * @param yFocus      Échelle en Y utilisée avec le graphique "focus".
+ * @param yContext    Échelle en Y utilisée avec le graphique "contexte".
+ * @param sources     Données triées par nom de rue et par date (voir fonction "createSources").
+ */
+function domainY(yFocus, yContext, sources) {
+  // TODO: Préciser les domaines pour les variables "yFocus" et "yContext" pour l'axe Y.
+
+}
